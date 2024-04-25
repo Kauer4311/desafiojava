a25abc57e18140d36d0295771f1a3500fe2a86cf
Kauer4311 <156729485+Kauer4311@users.noreply.github.com>
2024-04-25 18:44:44 -0300

    Add files via upload

---
+++ desafio java/script.js
@@ -0,0 +1,25 @@
+let firstNumber=prompt("digite um numero para multiplicação divisão subtração soma e o resto da divisão")
+let  twoNumber=prompt("digite o segundo número")
+
+firstNumber= Number (firstNumber)
+twoNumber= Number (twoNumber)
+
+ 
+
+const sum=  firstNumber + twoNumber
+
+const sub= firstNumber - twoNumber
+
+const div= firstNumber / twoNumber
+
+const mult= firstNumber* twoNumber
+
+const restdiv= firstNumber % twoNumber
+
+alert('Soma:'+ sum)
+alert('Subtração:'+ sub)
+alert('divisão:'+ div)
+alert('multiplicação:'+ mult)
+alert('resto da divisão:'+ restdiv)
+
+
